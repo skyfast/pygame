b6e8faa586b9c23dec03c9aebef08ab55dc96698
“skyfast” <theskyfast@yahoo.com>
2021-10-12 19:43:45 -0700

    added game over

--- main.py
+++ main.py
@@ -39,9 +39,19 @@ e4 = enemy.Enemy(random.randint(0, 735), 90)
 e5 = enemy.Enemy(random.randint(0, 735), 90)
 enemy_list = [e1, e2, e3, e4, e5]
 
+
 def show_score():
     score = font.render("Score = {}".format(str(score_vale)), True, (255, 255, 255))
     screen.blit(score, (text_x, text_y))
+
+
+# Game Over
+over_font = pygame.font.Font('freesansbold.ttf', 64)
+
+
+def game_over():
+    over_text = over_font.render("GAME OVER", True, (255, 255, 255))
+    screen.blit(over_text, (200, 250))
 
 
 p = player.Player()
@@ -50,45 +60,60 @@ dt = 0
 prev_time = time.time()
 # game loop
 running = True
+in_game = True
 while running:
-    now = time.time()
-    dt = now - prev_time
-    prev_time = now
-    # check all game events
-    for event in pygame.event.get():
-        # quits the game
-        if event.type == pygame.QUIT:
-            running = False
+        while in_game:
+            now = time.time()
+            dt = now - prev_time
+            prev_time = now
+            # check all game events
+            for event in pygame.event.get():
+                # quits the game
+                if event.type == pygame.QUIT:
+                    running = False
+                    in_game = False
 
-        # check to see if the current key is released
-        if event.type == pygame.KEYUP:
-            if (event.key == pygame.K_LEFT and last_key_down == 'left') or (event.key == pygame.K_RIGHT and
-                                                                            last_key_down == 'right'):
-                p.moving = "none"
-        # checks to see if there was a key press
-        if event.type == pygame.KEYDOWN:
-            if event.key == pygame.K_LEFT:
-                p.moving = "left"
-                last_key_down = 'left'
-            if event.key == pygame.K_RIGHT:
-                p.moving = "right"
-                last_key_down = 'right'
-            if event.key == pygame.K_SPACE:
-                p.shoot()
+                # check to see if the current key is released
+                if event.type == pygame.KEYUP:
+                    if (event.key == pygame.K_LEFT and last_key_down == 'left') or (event.key == pygame.K_RIGHT and
+                                                                                    last_key_down == 'right'):
+                        p.moving = "none"
+                # checks to see if there was a key press
+                if event.type == pygame.KEYDOWN:
+                    if event.key == pygame.K_LEFT:
+                        p.moving = "left"
+                        last_key_down = 'left'
+                    if event.key == pygame.K_RIGHT:
+                        p.moving = "right"
+                        last_key_down = 'right'
+                    if event.key == pygame.K_SPACE:
+                        p.shoot()
 
-    screen.fill((0, 0, 0))
-    # Background
-    screen.blit(background, (0, 0))
-    p.move(dt)
+            screen.fill((0, 0, 0))
+            # Background
+            screen.blit(background, (0, 0))
+            p.move(dt)
 
-    for e in enemy_list:
-        e.move(dt)
-        if p.check_hit(e.x_cord, e.y_cord):
-            e.x_cord = random.randint(0, 735)
-            e.y_cord = 50
-            score_vale += 1
-        e.draw(screen)
+            for e in enemy_list:
+                e.move(dt)
+                if p.check_hit(e.x_cord, e.y_cord):
+                    e.x_cord = random.randint(0, 735)
+                    e.y_cord = 50
+                    score_vale += 1
+                if e.y_cord > 400:
+                    in_game = False
+                e.draw(screen)
 
-    p.draw(screen)
-    show_score()
-    pygame.display.update()
+            p.draw(screen)
+            show_score()
+            pygame.display.update()
+
+
+        for event in pygame.event.get():
+            # quits the game
+            if event.type == pygame.QUIT:
+                running = False
+        p.draw(screen)
+        game_over()
+        show_score()
+        pygame.display.update()
